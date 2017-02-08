9329d2f2566e61291c6b949e60081c241b9f2fa4
dnicolauv <ulin666@gmail.com>
2017-02-08 14:38:31 +0100

    initial commit

---
+++ ToDo.Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace TodoApi
+{
+	public class Program
+	{
+		protected static void Main(string[] args)
+		{
+			var port = 8080;
+			var url = $"http://*:{port}";
+			using (WebApp.Start<Startup>("http://*:8080/"))
+			{
+				Console.WriteLine($"Web Server is running at {url}.");
+				Console.WriteLine("Press any key to quit.");
+				Console.ReadLine();
+			}
+		}
+	}
+}
